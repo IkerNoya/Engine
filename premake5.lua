fef7102acbd1586d65666716ca02db106486c7e1
Iker Noya <ikernoya4@gmail.com>
2022-03-24 19:36:56 -0300

    Added project generation with premake

---
+++ premake5.lua
@@ -0,0 +1,113 @@
+workspace "Engine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir	= "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("Bin/"  ..outputdir.. "/%{prj.name}")
+	objdir ("Obj/"  ..outputdir.. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"%{prj.name}//Libs/spdlog/include"
+	}
+	
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"DIV_BUILD_DLL"	
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../Bin/" .. outputdir .. "/Game")
+		}
+
+	filter "configurations:Debug"
+		defines "DIV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DIV_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DIV_DIST"
+		optimize "On"
+
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"
+		optimize "On"
+
+project "Game"
+	location "Game"
+	kind "ConsoleApp"
+
+	language = "C++"
+
+	targetdir ("Bin/"  ..outputdir.. "/%{prj.name}")
+	objdir ("Obj/"  ..outputdir.. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"Engine/Libs/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+	
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			
+		}
+
+	filter "configurations:Debug"
+		defines "DIV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DIV_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DIV_DIST"
+		optimize "On"
+
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"
+		optimize "On"
